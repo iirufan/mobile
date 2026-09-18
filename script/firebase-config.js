26a7aa83ea7a4f64970088e9fff61e6aa7c2f1d2
iirufan <42711386+iirufan@users.noreply.github.com>
2026-09-18 10:25:37 +0500

    Add Firebase configuration file

---
+++ script/firebase-config.js
@@ -0,0 +1,12 @@
+// Copy your Firebase web config here. Keep this file as script/firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyCJJ6hH_MeQLVXBvNYgPfbn1BDb2ZYUpUk",
+  authDomain: "ktest-19183.firebaseapp.com",
+  databaseURL: "https://ktest-19183-default-rtdb.firebaseio.com",
+  projectId: "ktest-19183",
+  storageBucket: "ktest-19183.firebasestorage.app",
+  messagingSenderId: "361705910072",
+  appId: "1:361705910072:web:c4819fd57fc8373013dbcd",
+  measurementId: "G-0H52BF6RZF"
+};
+if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
