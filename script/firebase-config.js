e1f29f4bba901f30d055ff3607d8068807ee56b5
iirufan <42711386+iirufan@users.noreply.github.com>
2026-09-18 10:26:51 +0500

    Update Firebase configuration settings

--- script/firebase-config.js
+++ script/firebase-config.js
@@ -1,12 +1,12 @@
 // Copy your Firebase web config here. Keep this file as script/firebase-config.js
 const firebaseConfig = {
-  apiKey: "AIzaSyCJJ6hH_MeQLVXBvNYgPfbn1BDb2ZYUpUk",
-  authDomain: "ktest-19183.firebaseapp.com",
-  databaseURL: "https://ktest-19183-default-rtdb.firebaseio.com",
-  projectId: "ktest-19183",
-  storageBucket: "ktest-19183.firebasestorage.app",
-  messagingSenderId: "361705910072",
-  appId: "1:361705910072:web:c4819fd57fc8373013dbcd",
-  measurementId: "G-0H52BF6RZF"
+  apiKey: "AIzaSyBJe4nvC4Q2p-lBSth8a4LOSkWIwW8Zs2g",
+  authDomain: "housekeeping-791a4.firebaseapp.com",
+  databaseURL: "https://housekeeping-791a4-default-rtdb.firebaseio.com",
+  projectId: "housekeeping-791a4",
+  storageBucket: "housekeeping-791a4.firebasestorage.app",
+  messagingSenderId: "451334815894",
+  appId: "1:451334815894:web:df851c38bd22b6094382b7",
+  measurementId: "G-2NQHXVSSRR"
 };
 if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
