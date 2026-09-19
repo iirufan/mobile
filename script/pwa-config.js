af32ac0ee862d70eda6f654a528b0277f83a0601
iirufan <42711386+iirufan@users.noreply.github.com>
2026-09-19 11:06:29 +0500

    Update VAPID key in PWA configuration

--- script/pwa-config.js
+++ script/pwa-config.js
@@ -2,5 +2,5 @@
 // Firebase Console -> Project settings -> Cloud Messaging -> Web Push certificates.
 // Paste the PUBLIC VAPID key below. This is safe to keep in GitHub.
 window.KOVELI_PWA_CONFIG = {
-  vapidKey: "PASTE_YOUR_FIREBASE_WEB_PUSH_PUBLIC_VAPID_KEY_HERE"
+  vapidKey: "BGbE0OukJGUhwiM62dixk6DIGxq0jOtsFHHNlqF98VlmApl6b9FiiXtfQGbQl3EcvYpSRuEZCTK98eMvQelAskM"
 };
